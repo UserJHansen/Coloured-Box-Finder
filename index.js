88633de1c61786f155ebf449beb50d87caed59e0
JHansen <47265397+UserJHansen@users.noreply.github.com>
2021-11-06 21:35:01 +1000

    Finish Single Threaded Mode

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
-/*!
- * This file is part of Rembrandt.js
- * Copyright (c) 2016 PhotoEditorSDK.com
- * Licensed under MIT license (https://opensource.org/licenses/MIT)
- * @license
- */
-
-import fs from 'fs'
-
-import Utils from './lib/utils'
-import Promise from './vendor/promise'
-import Constants from './constants'
-import Image from './lib/image'
-import Color from './lib/color'
-import ImageComparator from './lib/image-comparator'
-
-// @ifdef BROWSER
-if (typeof BROWSER !== 'undefined') {
-  global.Buffer = () => {}
-}
-// @endif
+import Utils from "./lib/utils";
+import Constants from "./constants";
+import Image from "./lib/image";
+import Color from "./lib/color";
+import ImageComparator from "./lib/image-comparator";
 
 class Rembrandt {
-  constructor (options) {
-    this._imageA = null
-    this._imageB = null
-
+  constructor(options) {
     this._options = Utils.defaults(options, {
+      targetColors: [{ r: 0, g: 0, b: 0, a: 1 }],
       imageA: null,
-      imageB: null,
       thresholdType: Rembrandt.THRESHOLD_PERCENT,
       maxThreshold: 0.01,
       maxDelta: 0.02,
-      renderComposition: false,
       compositionMaskColor: Color.RED,
-      maxOffset: 0
-    })
+      maxOffset: 0,
+    });
 
-    this._validateOptions()
+    this._imageA = this._options.imageA;
   }
 
   // -------------------------------------------------------------------------- PUBLIC API
@@ -45,12 +25,18 @@ class Rembrandt {
    * Compares the input images
    * @return {Promise}
    */
-  compare () {
-    return this._loadImages()
-      .then(() => {
-        const comparator = new ImageComparator(this._imageA, this._imageB, this._options)
-        return comparator.compare()
-      })
+  compare() {
+    this._imageA.width = this._imageA._context.canvas.width;
+    this._imageA.height = this._imageA._context.canvas.height;
+    this._imageA._imageData = this._imageA._context.getImageData(
+      0,
+      0,
+      this._imageA.width,
+      this._imageA.height
+    );
+
+    const comparator = new ImageComparator(this._imageA, this._options);
+    return comparator.compare();
   }
 
   // -------------------------------------------------------------------------- STATIC PUBLIC API
@@ -61,102 +47,18 @@ class Rembrandt {
    * @param  {Number} height
    * @return {Rembrandt.Image}
    */
-  static createImage (width, height) {
-    return new Image(width, height)
-  }
-
-  // -------------------------------------------------------------------------- PRIVATE API
-
-  /**
-   * Validates the options
-   * @private
-   */
-  _validateOptions () {
-    // Image options validation
-    const checkImageValid = (optionName) => {
-      const image = this._options[optionName]
-      if (!(typeof image === 'string' || Buffer.isBuffer(image) || image instanceof Image)) {
-        throw new Error(`Option \`${optionName}\` must either be a String, Buffer or Rembrandt.Image.`)
-      }
-    }
-    checkImageValid('imageA')
-    checkImageValid('imageB')
-
-    const { thresholdType, threshold } = this._options
-
-    // Threshold type validation
-    const validThresholdTypes = [Rembrandt.THRESHOLD_PERCENT, Rembrandt.THRESHOLD_PIXELS]
-    if (validThresholdTypes.indexOf(thresholdType) === -1) {
-      throw new Error(
-        '`thresholdType` must be either Rembrandt.THRESHOLD_PERCENT or Rembrandt.THRESHOLD_PIXELS'
-      )
-    }
-
-    // Threshold validation
-    if (thresholdType === Rembrandt.THRESHOLD_PERCENT && (threshold < 0 || threshold > 1)) {
-      throw new Error('`threshold` must be between 0 and 1')
-    }
-  }
-
-  /**
-   * Loads the images
-   * @private
-   */
-  _loadImages () {
-    return this._loadImage(this._options.imageA)
-      .then((imageA) => {
-        this._imageA = imageA
-        return this._loadImage(this._options.imageB)
-      })
-      .then((imageB) => {
-        this._imageB = imageB
-      })
-  }
-
-  /**
-   * Loads the given image
-   * @param  {String|Buffer} image
-   * @return {Buffer}
-   * @private
-   */
-  _loadImage (image) {
-    return new Promise((resolve, reject) => {
-      if (image instanceof Image) {
-        return resolve(image)
-      }
-
-      if (image instanceof Buffer) {
-        return resolve(Image.fromBuffer(image))
-      }
-
-      // @ifndef BROWSER
-      fs.readFile(image, (err, buf) => {
-        if (err) return reject(err)
-        resolve(Image.fromBuffer(image))
-      })
-      // @endif
-      // @ifdef BROWSER
-      if (typeof BROWSER !== 'undefined') {
-        const browserImage = Utils.createImage()
-        browserImage.addEventListener('load', () => {
-          resolve(Image.fromImage(browserImage))
-        })
-        browserImage.crossOrigin = 'Anonymous'
-        browserImage.src = image
-      }
-      // @endif
-    })
+  static createImage(width, height) {
+    return new Image(width, height);
   }
 }
+Rembrandt.Image = Image;
+Rembrandt.Color = Color;
 
-Rembrandt.Image = Image
-Rembrandt.Color = Color
-
-Rembrandt.version = require('./package.json').version
+Rembrandt.version = require("./package.json").version;
 
 // Copy constants to Rembrandt object
 for (let key in Constants) {
-  Rembrandt[key] = Constants[key]
+  Rembrandt[key] = Constants[key];
 }
 
-module.exports = Rembrandt
+module.exports = Rembrandt;
